0198b1a9d9e27837b096c7a41b006300e752226f
liubaozhu <liubaozhu@baidu.com>
2020-01-28 10:44:36 +0800

    add something

---
+++ app/src/main/java/com/android/liubz/androidtea/java/annotation/InheritableSon.java
@@ -0,0 +1,15 @@
+package com.android.liubz.androidtea.java.annotation;
+
+/**
+ * Created by liubaozhu on 2020-01-17
+ */
+public class InheritableSon extends InheritableFather {
+
+    public InheritableSon() {
+        System.out.println("InheritableSon: " + InheritableSon.class.isAnnotationPresent(MyAnnotation.class));
+    }
+
+    public static void main(String[] args) {
+        InheritableSon is = new InheritableSon();
+    }
+}
