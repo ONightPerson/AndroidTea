fb523a935a9379d13fd2e67140c34109f7df261e
liubaozhu <liubaozhu@baidu.com>
2020-02-06 23:24:20 +0800

    [master]sort

--- app/src/main/java/com/android/liubz/androidtea/java/interview/sort/SortUtils.java
+++ app/src/main/java/com/android/liubz/androidtea/java/interview/sort/SortUtils.java
@@ -10,7 +10,9 @@ public class SortUtils {
 //        quickSort(new int[] {5, 8, 9, 3, 6});
 //        insertionSort(new int[] {5, 8, 9, 3, 6});
 //        shellSort(new int[] {5, 8, 9, 3, 6});
-        mergeSort(new int[] {5, 8, 9, 3, 6});
+//        mergeSort(new int[] {5, 8, 9, 3, 6});
+//        heapSort(new int[] {5, 8, 9, 3, 6});
+        countingSort(new int[] {5, 8, 9, 3, 6});
     }
 
     /**
@@ -190,6 +192,95 @@ public class SortUtils {
         }
         return newArr;
     }
+
+    /**
+     * 堆排序，降序排列（小顶堆）
+     * @param arr
+     */
+    public static void heapSort(int[] arr) {
+        if (arr == null) {
+            return;
+        }
+        // 先调整对，形成小顶堆
+        int len = arr.length;
+        int finNoLeaf = len/2 - 1;
+        for (int i = finNoLeaf; i >= 0; i--) {
+           simplyHeapify(arr, i, len);
+        }
+        for (int i = len - 1; i > 0; i--) {
+            swap(arr, 0, i);
+            simplyHeapify(arr, 0, i);
+        }
+        output(arr);
+    }
+
+    private static void simplyHeapify(int[] arr, int node, int len) {
+        int tmp = arr[node];
+        for (int i = 2 * node + 1; i < len; i = 2 * i + 1) {
+            if (i+1 < len && arr[i+1] < arr[i]) {
+                i++;
+            }
+            if (tmp > arr[i]) {
+                arr[node] = arr[i];
+                node = i;
+            } else {
+                break;
+            }
+        }
+        arr[node] = tmp;
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+
+    /**
+     * 计数排序，降序排列
+     * @param arr 原数组
+     */
+    public static void countingSort(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            return;
+        }
+        int[] minMax = getMinMax(arr);
+        int min = minMax[0];
+        int max = minMax[1];
+        doCountingSort(arr, min, max);
+        output(arr);
+    }
+
+    private static void doCountingSort(int[] arr, int min, int max) {
+        int bucketLen = max - min + 1;
+        int[] bucket = new int[bucketLen];
+        for (int value : arr) {
+            bucket[value - min]++;
+        }
+
+        int sortedIndex = 0;
+        for (int i = bucketLen - 1; i >= 0; i--) {
+            while (bucket[i] > 0) {
+                arr[sortedIndex++] = i + min;
+                bucket[i]--;
+            }
+        }
+    }
+
+    private static int[] getMinMax(int[] arr) {
+        int min, max;
+        min = max = arr[0];
+        int len = arr.length;
+        for (int i = 1; i < len; i++) {
+            if (min > arr[i]) {
+                min = arr[i];
+            } else if (max < arr[i]) {
+                max = arr[i];
+            }
+        }
+        return new int[] {min, max};
+    }
+
 
     private static void output(int[]  input) {
         System.out.println(Arrays.toString(input));
