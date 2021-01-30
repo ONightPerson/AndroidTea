11e041c8c7543de8c8ec547004eacc0f414f92e0
liubaozhu <liubaozhu@baidu.com>
2021-01-30 09:48:09 +0800

    udp receiver

--- cherish/src/main/java/com/liubz/androidtea/cherish/net/UdpReceiver.java
+++ cherish/src/main/java/com/liubz/androidtea/cherish/net/UdpReceiver.java
@@ -12,20 +12,20 @@ public class UdpReceiver {
 
     public static void main(String[] args) throws IOException {
         DatagramSocket socket = new DatagramSocket(13333);
-        byte[] buffer = new byte[1024];
-        DatagramPacket packet = new DatagramPacket(buffer, 0, buffer.length);
-        System.out.println("waiting receiving");
-        long startTime = System.currentTimeMillis();
-        socket.receive(packet);
-        System.out.println("finish receiving packet, time: "
-                + (System.currentTimeMillis() - startTime));
-        String ip = packet.getAddress().getHostAddress();
-        byte[] data = packet.getData();
-        System.out.println("data size: " + data.length);
-        String content = new String(packet.getData(), 0, packet.getLength());
-        int port = packet.getPort();
-        System.out.println(ip + ":" + port + ", data: " + content);
-
+        while (!Thread.interrupted()) {
+            byte[] buffer = new byte[1024];
+            DatagramPacket packet = new DatagramPacket(buffer, 0, buffer.length);
+            long startTime = System.currentTimeMillis();
+            socket.receive(packet);
+            System.out.println("finish receiving packet, time: "
+                    + (System.currentTimeMillis() - startTime));
+            String ip = packet.getAddress().getHostAddress();
+            byte[] data = packet.getData();
+            System.out.println("data size: " + data.length);
+            String content = new String(packet.getData(), 0, packet.getLength());
+            int port = packet.getPort();
+            System.out.println(ip + ":" + port + ", data: " + content);
+        }
         socket.close();
     }
 }
