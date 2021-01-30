d06325ed79470a420bf7baa1abf8c7755ddc0ecf
liubaozhu <liubaozhu@baidu.com>
2021-01-30 11:19:56 +0800

    tcp

--- cherish/src/main/java/com/liubz/androidtea/cherish/net/TcpClient.java
+++ cherish/src/main/java/com/liubz/androidtea/cherish/net/TcpClient.java
@@ -1,7 +1,12 @@
 package com.liubz.androidtea.cherish.net;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.Socket;
 
 /**
@@ -10,10 +15,94 @@ import java.net.Socket;
  */
 public class TcpClient {
 
+    private Socket mClient;
+    private BufferedReader mReader;
+    private BufferedWriter mWriter;
+
     public static void main(String[] args) throws IOException {
-        Socket client = new Socket("192.168.3.104", 13333);
-        OutputStream os = client.getOutputStream();
-        os.write("您好".getBytes());
-        client.close();
+        TcpClient client = new TcpClient();
+        client.init();
+
+        client.startWriting();
+        client.startReading();
+    }
+
+    private void init() throws IOException {
+        mClient = new Socket( "192.168.3.104", 13333);
+    }
+
+    private void startWriting() throws IOException {
+        OutputStream os = mClient.getOutputStream();
+        mWriter = new BufferedWriter(new OutputStreamWriter(os));
+        new Thread(new WriteTask(mWriter)).start();
+    }
+
+    private void startReading() throws IOException {
+        InputStream is = mClient.getInputStream();
+        mReader = new BufferedReader(new InputStreamReader(is));
+        new Thread(new ReadTask(mReader)).start();
+    }
+
+    private static class ReadTask implements Runnable {
+        private BufferedReader reader;
+
+        public ReadTask(BufferedReader reader) {
+            this.reader = reader;
+        }
+
+        @Override
+        public void run() {
+            String line;
+            while (true) {
+                try {
+                    if (!((line = reader.readLine()) != null)) {
+                        break;
+                    }
+                    System.out.println(line);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        reader.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+        }
+    }
+
+    private static class WriteTask implements Runnable {
+
+        private BufferedWriter writer;
+
+        public WriteTask(BufferedWriter writer) {
+            this.writer = writer;
+        }
+
+        @Override
+        public void run() {
+            BufferedReader localReader = new BufferedReader(new InputStreamReader(System.in));
+            String line;
+            while (true) {
+                try {
+                    if (((line = localReader.readLine()) == null)) {
+                        break;
+                    }
+                    writer.write(line);
+                    writer.newLine();
+                    writer.flush();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        writer.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
     }
 }
--- cherish/src/main/java/com/liubz/androidtea/cherish/net/TcpServer.java
+++ cherish/src/main/java/com/liubz/androidtea/cherish/net/TcpServer.java
@@ -1,9 +1,14 @@
 package com.liubz.androidtea.cherish.net;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * author: created by liubaozhu on 2021/1/30
@@ -12,15 +17,44 @@ import java.net.Socket;
 public class TcpServer {
 
     public static void main(String[] args) throws IOException {
+
+        ExecutorService service = Executors.newFixedThreadPool(5);
         // 建立TCP服务器，并监听13333 端口
         ServerSocket server = new ServerSocket(13333);
         Socket client = server.accept();
-        InputStream is = client.getInputStream();
-        byte[] buff = new byte[1024];
-        int len = is.read(buff);
-        System.out.println("data" + new String(buff, 0, len) + ", byte number: " + len);
+        service.execute(new IOTask(client));
+    }
 
-        client.close();
-        server.close();
+    private static class IOTask implements Runnable {
+
+        private Socket client;
+
+        public IOTask(Socket client) {
+            this.client = client;
+        }
+
+        @Override
+        public void run() {
+            try {
+                doTask();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        private void doTask() throws IOException {
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
+            BufferedReader reader = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            String line;
+            while (((line = reader.readLine()) != null)) {
+                System.out.println(line);
+                writer.write("收到你的消息：\"" + line + "\".");
+                writer.newLine();
+                writer.flush();
+            }
+            writer.close();
+            reader.close();
+            client.close();
+        }
     }
 }
