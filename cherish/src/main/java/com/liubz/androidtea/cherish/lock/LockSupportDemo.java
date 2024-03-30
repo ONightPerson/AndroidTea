bed26d9086b3411b8d4138a548c7fa477764ac9e
liubaozhu <yuenuanlantian@gmail.com>
2024-03-30 09:19:10 +0800

    something

---
+++ cherish/src/main/java/com/liubz/androidtea/cherish/lock/LockSupportDemo.java
@@ -0,0 +1,14 @@
+package com.liubz.androidtea.cherish.lock;
+
+import java.util.concurrent.locks.LockSupport;
+
+public class LockSupportDemo {
+
+    public void testPark() {
+        LockSupport.park(this);
+    }
+    public static void main(String[] args) {
+        LockSupportDemo demo = new LockSupportDemo();
+        demo.testPark();
+    }
+}
