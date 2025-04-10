619036e1e2d14d4caa67a2392914f827d3b3fea0
liubaozhu02 <liubaozhu02@meituan.com>
2025-04-10 15:25:43 +0800

    feat: 希尔排序

--- cherish/src/main/java/com/liubz/androidtea/cherish/algorithm/sort/ShellSort.java
+++ cherish/src/main/java/com/liubz/androidtea/cherish/algorithm/sort/ShellSort.java
@@ -1,4 +1,29 @@
 package com.liubz.androidtea.cherish.algorithm.sort;
 
 public class ShellSort {
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 2, 5, 4};
+        shellSort(arr);
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+
+    static void shellSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        int n = arr.length;
+        for (int gap = n / 2; gap > 0; gap /= 2) {
+            for (int i = gap; i < n; i += gap) {
+                int temp = arr[i]; // 待排序
+                int j = i - gap; // 已排序的最后索引
+                while (j >= 0 && arr[j] > temp) {
+                    arr[j + gap] = arr[j];
+                    j -= gap;
+                }
+                arr[j + gap] = temp;
+            }
+        }
+    }
 }
