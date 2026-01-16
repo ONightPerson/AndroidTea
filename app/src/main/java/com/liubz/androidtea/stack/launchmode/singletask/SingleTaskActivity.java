21d2d053235f85e3a04e612ed336376ea37efc5d
liubaozhu <yuenuanlantian@gmail.com>
2026-01-16 15:20:32 +0800

    singleTask

--- app/src/main/java/com/liubz/androidtea/stack/launchmode/singletask/SingleTaskActivity.java
+++ app/src/main/java/com/liubz/androidtea/stack/launchmode/singletask/SingleTaskActivity.java
@@ -1,7 +1,10 @@
 package com.liubz.androidtea.stack.launchmode.singletask;
 
+import static com.liubz.androidtea.stack.launchmode.LaunchModeConst.LAUNCH_MODE_TAG_SUFFIX;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
 
@@ -15,6 +18,7 @@ import com.liubz.androidtea.stack.launchmode.OtherActivity;
  * @Date: 2023/2/17 7:11 PM
  */
 public class SingleTaskActivity extends BaseActivity {
+    private static final String TAG = "SingleTaskActivity" + LAUNCH_MODE_TAG_SUFFIX;
     private ActivitySingleTaskBinding binding;
 
     @Override
@@ -25,6 +29,12 @@ public class SingleTaskActivity extends BaseActivity {
         setTitle("SingleTaskActivity");
         
         initListeners();
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        Log.i(TAG, "onNewIntent");
     }
 
     private void initListeners() {
